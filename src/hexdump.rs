5cf1a375760b1a6bcf793c59b47e8737c6a98e67
Matteo Mazzarella <matteo.mazzarella@ikon.it>
2025-06-07 19:21:24 +0200

    hexdump improvements

--- src/hexdump.rs
+++ src/hexdump.rs
@@ -1,15 +1,32 @@
 pub fn dump(buf: &[u8]) {
-    let bytes_per_line = 0xf;
+    let bytes_per_line = 0x10;
 
     for (i, chunk) in buf.chunks(bytes_per_line).enumerate() {
         print!("{:08x}: ", i * bytes_per_line);
 
+        let mut i = 0;
         for byte in chunk.iter() {
             print!("{:02x} ", byte);
+            i += 1;
+
+            if i % 8 == 0 {
+                print!(" ");
+            }
         }
 
-        for _ in 0..(bytes_per_line - chunk.len()) {
+        let nchunks = chunk.len();
+        let nleft = bytes_per_line - nchunks;
+
+        for i in 0..nleft {
             print!("   ");
+
+            if (nchunks + i) % 8 == 0 {
+                print!(" ");
+            }
+
+            if i == nleft - 1 {
+                print!(" ");
+            }
         }
 
         print!("|");
